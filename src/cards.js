256066d61c358e9569274a1ecbec73e35d02cbe8
EatCakeAndDie <61690317+EatCakeAndDie@users.noreply.github.com>
2020-11-24 22:14:05 +0000

    load cards progress

--- src/cards.js
+++ src/cards.js
@@ -8,16 +8,20 @@ import { el, element } from './lib/utils.js';
 function testCards(videoData) {
   const children = loadCards(3, videoData);
   const testGrid = element('div', { class: 'grid' }, null,
-    element('div', { class: 'row' }, null,
-      children
-    )
+    element('div', { class: 'row' }, null, null)
   );
   const main = document.querySelector('main');
   main.appendChild(testGrid);
+  const gridDiv = document.querySelector('div[class=grid]');
+  for(let i = 0; i < children.length ; i++) {
+    gridDiv.appendChild(children[i]);
+  }
 }
 
 function loadCards(noOfCards, videoData) {
+  const childArray = [];
   for (let i = 0; i < noOfCards; i++) {
+    childArray.push(
       element('div', { class: 'card col col-4' }, null,
         element('div', { class: 'image' }, null,
           element('img', {src: videoData.videos[i].poster }, null, "skil ekki afhverju það þarf að vera eitthvað hér"),
@@ -27,8 +31,10 @@ function loadCards(noOfCards, videoData) {
           element('h3', null, null, videoData.videos[i].title),
           element('div', { class: 'c-counter' }, null, "fyrirsidan")
         )
-      );
+      )
+    );
   }
+  return childArray;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
