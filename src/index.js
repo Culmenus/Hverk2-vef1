9846b7431a88635f042188b92cc8a6f79ac8a3e5
unknown <tfj2@hi.is>
2020-11-24 18:34:27 +0000

    function skila streng

--- src/index.js
+++ src/index.js
@@ -10,21 +10,28 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 function process(videodata) {
-  time(videodata.videos[1]);
-  lengthOfVid(videodata.videos[1]);
+  var theTime = time(videodata.videos[1]);
+  var theLength = lengthOfVid(videodata.videos[1]);
+  console.log(theTime);
+  console.log(theLength);
 }
 
+/**
+ * Use: lengthOfVid(Array)
+ * Pre: Array er array úr jason skránni, specifically videos tilvik
+ * Result: Skilar streng með duration á 0:00 formi, breytir úr sek.
+ */
 function lengthOfVid(VideoDataVideos){
 
   var time = VideoDataVideos.duration;
   console.log(time);
   if(time < 10){
     let theTime = "0:0" + time;
-    console.log(theTime);
+    return theTime;
   }
   else if(time>9 && time<60){
     let theTime = "0:" + time;
-    console.log(theTime);
+    return theTime;
   }
   else if(time>59){
    theTime = "" + Math.floor(time/60) + ":";
@@ -34,11 +41,15 @@ function lengthOfVid(VideoDataVideos){
    else{
      theTime = theTime + time%60;
    }
-   console.log(theTime);
+   return theTime;
   }
 }
-///tekur inn videos jason array og skilar tímanum frá því að
-///það var created.
+
+/**
+ * Use: time(Array)
+ * Pre: Array er array úr jason skránni, specifically videos tilvik
+ * Result: Skilar streng með hversu langt síðan myndbandið var búið til.
+ */
 function time(videodataVideos){
   var d = new Date();
   var n = d.getTime();
@@ -47,33 +58,45 @@ function time(videodataVideos){
   time = Math.round(time);
 
   if(time <2){
-    console.log("Fyrir " + 1 + " klukkustund síðan");
+    var LongAgo =  ("Fyrir " + 1 + " klukkustund síðan");
+    return LongAgo;
   }
   else if(time>2 && time<48){
-    console.log("Fyrir " + time + " klukkustundum síðan");
+    var LongAgo = ("Fyrir " + time + " klukkustundum síðan");
+    return LongAgo;
   }
   else if(time<48 && time>23){
-    console.log("Fyrir " + 1 + " degi síðan");
+    var LongAgo = ("Fyrir " + 1 + " degi síðan");
+    return LongAgo;
   }
   else if(time>48 && time<168){
     console.log("Fyrir " + Math.round(time/24) + " dögum síðan");
   }
   else if(time>167 && time<(168*2)){
-    console.log("Fyrir " + 1 + " viku síðan");
+    var LongAgo = ("Fyrir " + 1 + " viku síðan");
+    return LongAgo;
   }
   else if(time>(168*2) && time<720){
-    console.log("Fyrir " + Math.round((time/24)/7) + " vikum síðan");
+    var LongAgo = ("Fyrir " + Math.round((time/24)/7) + " vikum síðan");
+    return LongAgo;
   }
   else if(time>720 && time<(720*2)){
-    console.log("Fyrir " + 1 + " mánuði síðan");
+    var LongAgo = ("Fyrir " + 1 + " mánuði síðan");
+    return LongAgo;
   }
   else if(time>(720*2) && time<8760){
-    console.log("Fyrir " + Math.round(((time/24)/30)) + " mánuðum síðan");
+    var LongAgo = ("Fyrir " + Math.round(((time/24)/30)) + " mánuðum síðan");
+    return LongAgo;
   }
   else if(time>8760 && time<(8760*2)){
-    console.log("Fyrir " + 1 + " ári síðan");
+    var LongAgo = ("Fyrir " + 1 + " ári síðan");
+    return LongAgo;
   }
   else if(time>(8760*2)){
-    console.log("Fyrir " + Math.round((time/24)/365) + " árum síðan");
+    var LongAgo = ("Fyrir " + Math.round((time/24)/365) + " árum síðan");
+    return LongAgo;
+  }
+  else{
+    return "";
   }
 }
